9ccea0a57319b3c0aae5d3d67419a91db7ad9315
Bohan Zhou <bohan@kth.se>
2019-03-13 15:36:20 +0100

    Finished sink class

--- WSNworld/Sink.m
+++ WSNworld/Sink.m
@@ -4,33 +4,34 @@ classdef Sink
     %  packets recieved
     
     properties
-        Value
+        pos; % Position of mobile sink
+        packRecieved; % Amount of packets recieved by sink
     end
     
     methods
         function obj = Sink(sizex, sizey) 
-            %Construct an instance of the class Sink 
+            %Constructor: Constructs an instance of the class Sink 
             %  Input: sizex = WSN size, x 
             %         sizey = WSN size, y
             %  This method creates an instance of this class
                         
             obj.pos = [sizex/2, sizey/2] ; % Initialize by placing sink in the middle of the WSN
-            obj.packRec = 0; % Initial packets recieved is zero 
+            obj.packRecieved = 0; % Initial packets recieved is zero 
         end
         
-        function sinkPos = move(obj, deltax, deltay)
+        function obj = move(obj, deltax, deltay)
             %METHOD Move
-            %   Input: obj = Sink object 
-            %          deltax = sink movement in x
+            %   Input: deltax = sink movement in x
             %          deltay = sink movement in y
             %   The method moves the sink and stores the updated position
             %   of the sink in obj.pos
-            %   return: sinkPos = New position of sink
-             sinkPos = [obj.pos(1)+deltax, obj.pos(2)+deltay];
-             obj.pos = sinkPos;
+            %   return: obj = Sink object 
+            
+            sinkPos = [obj.pos(1)+deltax, obj.pos(2)+deltay];
+            obj.pos = sinkPos;
         end
         
-        function packetsRec = packRec(packSize)
+        function obj = packRec(obj, packSize)
              %METHOD packRec
              %   Input: Packsize = Size of package that was recieved 
              
@@ -38,9 +39,11 @@ classdef Sink
              %   has recieved
              %   return: packetsRec = The total amount of packets that have
              %   been sent
-            obj.packRec = obj.packRec + packSize;
-            packetsRec = obj.packRate;
+            obj.packRecieved = obj.packRecieved + packSize;
+            %packetsRec = obj.packRecieved;
         end 
+        
+   
     end
 end
 
