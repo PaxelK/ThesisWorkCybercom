08633287987f4f967896ce72236053991c676034
Axel Karlsson <axel.karlsson@cybercom.com>
2019-05-07 11:29:40 +0200

    got 2ndLayer controller early prototype to work for one node

--- pyFiles/MPC/MPC2ndLayer.py
+++ pyFiles/MPC/MPC2ndLayer.py
@@ -17,7 +17,9 @@ class MPC2ndLayer(EnvironmentEngine):
         super().__init__()  
         
         self.verbose = False
-        
+        self.nrplots = 0
+         
+         
         self.m = GEKKO(remote=False)
         
         # time points
@@ -29,92 +31,68 @@ class MPC2ndLayer(EnvironmentEngine):
         self.const = 0.6
         self.packet = 1
         self.E = 1
+        self.pSize = ps
+        
         
         #Counter for plots
         self.nrplots = 1;
     
-    
-
-
-    def controlEnv(self):
+        # define distance 
+        self.dist = self.m.Var(value = 20)
         
         
-        
-        
-    
-"""    
-
-        # define distance
-        self.dist = self.m.Var(20)
-
+        self.v = self.m.MV(lb=-2,ub=2) # Upper bound should be something like sqrt(100**2 + 100**2)
+        self.v.STATUS = 1
+         
         # define data transmission rate
-        self.dtr = self.m.MV(value=self.PA, integer = True, lb=0,ub=20)
+        self.dtr = self.m.MV(integer = True, lb=0,ub=200)
         self.dtr.STATUS = 1
         
-        # define energy level
-        self.nrj_stored = self.m.Var(value = self.energy, lb = 0)
+         # energy to transmit
+        self.e = self.m.Intermediate(self.dtr*self.pSize*(Eelec + Eamp * self.dist**2))
         
-        # define how much data must be transmitted
-        amount = self.pSize*500
-        self.data = self.m.Var(value = amount)
-        self.ps = self.m.Var(value = self.PS, lb = 0)
+        self.data= self.m.Var()
         
-        # energy to transmit
-        self.e = self.m.Intermediate(((Eelec+EDA)*self.packet + self.dtr*self.pSize*(Eelec + Eamp * self.dist**2)) - self.Egen)
         
         # equations
         # track the position
         self.m.Equation(self.dist.dt() == self.v)
-        self.m.Equation(self.nrj_stored.dt() == -self.e)
         # as data is transmitted, remaining data stored decreases
         self.m.Equation(self.data.dt() == -self.dtr*self.pSize)
         
+         # soft (objective constraint)
+        self.final = self.m.Param(value=np.zeros(self.ctrlRes))
+        self.final.value[int(np.floor(self.ctrlRes/2)):-1] = 0.001
+        self.final.value[-1] = 1
         
-        # self.m.Equation(self.energy >= self.e)
+        self.m.Equation(self.final*(self.data)<=0)
         
         # objective
         self.m.Obj(self.e) # minimize energy
         
-        # soft (objective constraint)
-        self.final = self.m.Param(value=np.zeros(self.ctrlRes))
-        self.final.value[int(np.floor(self.ctrlRes/2)):-1] = 0.001
-        self.final.value[-1] = 1
-        #self.m.Equation(self.final*(self.data)<=0)
+        self.m.Obj(self.data*self.final)
         
-        #self.m.Obj(self.data*self.final)
-        
-        self.target = self.m.Intermediate(self.m.sqrt((self.data*self.final)**2))
-        self.m.Obj(self.target) # transmit data by the end
-        
-        
-        
-        # hard constraint
-        # this form may cause infeasibility if it can't achieve
-        # data=0 at the end
-        #self.m.fix(self.data,self.ctrlRes-1,0)
         # options
         self.m.options.IMODE = 6                # optimal control
         self.m.options.NODES = 3                # collocation nodes
         self.m.options.SOLVER = 1               # solver (IPOPT), 1 is for when integers is used as MV
         self.m.options.TIME_SHIFT = 1           # Setting for saving values from last solve()
-        
-        #self.m.solve(disp=False)
-        #self.setPR(self.dtr.value[0])
-        
-        
-        
+
+
+    def controlEnv(self):
+        self.m.solve()
         
     def plot(self):
         plt.figure(self.nrplots)
-        plt.subplot(6,1,1)
+        plt.subplot(5,1,1)
         plt.plot(self.m.time,self.dist.value,'r-',label='Distance')
         plt.legend()
         
-        plt.subplot(6,1,2)
+        plt.subplot(5,1,2)
         plt.plot(self.m.time,self.v.value,'k--',label='Velocity')
         plt.legend()
         
-        plt.subplot(6,1,3)
+        plt.subplot(5,1,3)
         plt.plot(self.m.time,self.e.value,'b-',label='Energy Consumption')
         plt.legend()
         
@@ -122,24 +100,28 @@ class MPC2ndLayer(EnvironmentEngine):
         #plt.plot(self.m.time,self.ps,'b-',label='Bits Sent')
         #plt.legend()
         
-        plt.subplot(6,1,4)
+        plt.subplot(5,1,4)
         plt.plot(self.m.time, self.data.value,'k.-',label='Data Remaining')
         plt.legend()
         
         
-        plt.subplot(6,1,5)
+        plt.subplot(5,1,5)
         plt.plot(self.m.time, self.dtr.value,'r-',label='Transmission Rate')
         plt.legend()
         
-        plt.subplot(6,1,6)
-        plt.plot(self.m.time,self.nrj_stored,'b-',label='Battery')
-        plt.legend()
-        plt.xlabel('Time')
+        #plt.subplot(6,1,6)
+        #plt.plot(self.m.time,self.nrj_stored,'b-',label='Battery')
+        #plt.legend()
+        #plt.xlabel('Time')
         
         self.nrplots+=1
         
+        
+        
+"""    
+             
     
-    def controlSink(self, velocity):  
+    def controlPR(self, velocity):  
         temp = np.float64(velocity)
         
         self.vp[0:] = self.v.value[1]
@@ -180,6 +162,7 @@ if __name__ == "__main__":
     testEnv = MPC2ndLayer(Hrz,Res)
     print(testEnv.sink.xPos)
     print(testEnv.ctrlHrz)
-        
+    testEnv.controlEnv()
+    testEnv.plot()
         
         
