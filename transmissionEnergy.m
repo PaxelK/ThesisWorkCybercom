c239f0f6f953a5b6fc475f9d9fc6da553ef0dd0c
axelkar <paxelkarlsson@gmail.com>
2019-07-15 21:30:33 +0200

    com

--- transmissionEnergy.m
+++ transmissionEnergy.m
@@ -11,6 +11,8 @@ d = [1:maxDist];
 
 conChildr = [1:maxDist];
 nrj = zeros(maxDist);
+nrj_dtr = zeros(maxDist);
+
 b = 1000;
 
 for i = 1:maxDist
@@ -21,9 +23,21 @@ for i = 1:maxDist
     end
 end
 
+conCh = 20;
+dtr = 1190:190:20000;
+ERx = conCh*((Eelec + EDA)*k);
+for it = 1:maxDist
+    for j = 1:maxDist
+        ETx = dtr(j)*(Eelec + Eamp * d(it)^2);
+        nrj_dtr(it, j) = ERx + ETx;
+    end
+end
+
 
 
 ppJ = (b.*ones(maxDist) + k.*ones(maxDist).*conChildr')./(k.*(nrj));
+
+ppJ_dtr = (dtr.*ones(maxDist) + k.*ones(maxDist).*conCh)./(k.*(nrj_dtr));
 
 figure(1)
 surfl(d, conChildr, nrj)
@@ -37,4 +51,18 @@ surfl(d, conChildr,ppJ)
 title('PPJ for Varying Distance and Connected Children')
 xlabel('Distance [m]')
 ylabel('Connected Children')
+zlabel('PPJ')
+
+figure(3)
+surfl(d, dtr,nrj_dtr)
+title('Energy Cost During Transmission Round for Varying Distance and Packets Transmitted')
+xlabel('Distance [m]')
+ylabel('Bits Transmitted')
+zlabel('PPJ')
+
+figure(4)
+surfl(d, dtr,ppJ_dtr)
+title('PPJ for Varying Distance and Packets Transmitted')
+xlabel('Distance [m]')
+ylabel('Bits Transmitted')
 zlabel('PPJ')
